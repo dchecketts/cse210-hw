a8f3120521ccbc7df407df51291f8d3d0434979e
Daniel Checketts <danielkchecketts@gmail.com>
2026-04-27 13:35:18 -0600

    Week 1 Homework pt. 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userAnswer = Console.ReadLine();
+        int percentAnswer = int.Parse(userAnswer);
+
+        string gradeLetter = "";
+
+        if (percentAnswer >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (percentAnswer >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (percentAnswer >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (percentAnswer >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+        
+        if (percentAnswer >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("That's rough buddy.");
+        }
     }
 }
